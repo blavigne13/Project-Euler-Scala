ba2406ef84abd419649ae8d60df05452251e5fff
blavigne13 <blavigne13@gmail.com>
2016-10-17 07:07:31 -0500

    added problem 5

---
+++ src/Problem5.scala
@@ -0,0 +1,28 @@
+/**
+ * Project Euler #5: Smallest number divisible by 1 through 20
+ * https://projecteuler.net/problem=5
+ */
+object Problem5 {
+  def main(args: Array[String]) {
+    println(listFact((2 to 10).toList).reduce(_ * _));
+    println(listFact((2 to 20).toList).reduce(_ * _));
+  }
+
+  /**
+   * Reduce a list of Int to its "essential" factors such that the product of
+   * those factors yields the smallest number divisible by each number in the
+   * input list.
+   */
+  def listFact(xs: List[Int]): List[Int] = xs match {
+    case Nil => Nil;
+    case x :: xs => x :: listFact(tailFact(xs, x));
+  }
+
+  /**
+   * Divide fact out of each element by which it is divisible.
+   */
+  def tailFact(xs: List[Int], fact: Int): List[Int] = xs match {
+    case Nil => Nil;
+    case x :: xs => (if (x % fact == 0) x / fact else x) :: tailFact(xs, fact);
+  }
+}
